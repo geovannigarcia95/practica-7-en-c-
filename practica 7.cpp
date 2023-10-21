06a7d254e1991c27680f6c7c35786055bd2ec9f4
geovanni <geovannipoker@gmail.com>
2023-10-20 19:14:03 -0600

    practica 7

---
+++ practica 7.cpp
@@ -0,0 +1,24 @@
+//7. Escriba un programa que calcule el valor de: 1+2+3+...+n.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+		int n, suma, i;
+	suma=0;
+	cout <<"numeros a sumar: ";
+	cin >>n;
+	for(i=1; i <= n ; i++)	
+	{
+		suma=suma+i;
+	}
+	cout <<"La suma de los numeros "<<n<< " numeros es: "<<suma<<endl;
+}
+
+
+
+                                   // PRACTICA 7 :)
+                                   
+                                   
+                                //YOSHIO GEOVANNI GARCIA LOPEZ
